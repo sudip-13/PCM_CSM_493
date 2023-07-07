b1df8a1f006ccc1926c75762325137fc11e74a81
Sudip Kundu <113622945+sudip-13@users.noreply.github.com>
2023-07-07 22:22:08 +0530

    Add files via upload

---
+++ arthematicop.java
@@ -0,0 +1,13 @@
+public class arthematicop {
+    void arthematic_op(){
+        float a=56,b=28;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+    }
+    public static void main(String[] args) {
+        arthematicop obj=new arthematicop();
+        obj.arthematic_op();
+    }
+}
