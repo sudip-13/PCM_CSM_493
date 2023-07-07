401a5be0c3d025a2fec2b0d9b5f1aac9e72daf06
Sudip Kundu <113622945+sudip-13@users.noreply.github.com>
2023-07-07 22:44:19 +0530

    Add files via upload

---
+++ ar_cir_rec_methodoverloading.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class overloarding_1{
+    void area(int l,int w){
+        int area=l*w;
+        System.out.println("Area of rectangle = "+area);
+    }
+    void area(int r){
+        double area=3.14*r*r;
+        System.out.println("Area of circle = "+area);
+    }
+}
+public class ar_cir_rec_methodoverloading {
+    public static void main(String [] args){
+        overloarding_1 obj=new overloarding_1();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter length and width of rectangle");
+        int l=sc.nextInt();
+        int w=sc.nextInt();
+        System.out.println("Enter radius of circle");
+        int r=sc.nextInt();
+        obj.area(r);
+        obj.area(l, w);
+    }
+}
