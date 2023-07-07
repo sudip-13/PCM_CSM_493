aaa51b6f8d8c5d394987b2884e602a99d189c7c0
Sudip Kundu <113622945+sudip-13@users.noreply.github.com>
2023-07-07 22:22:38 +0530

    Add files via upload

---
+++ airthematic.java
@@ -0,0 +1,9 @@
+public class airthematic {
+    public static void main(String[] args) {
+        float a=10,b=15;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
