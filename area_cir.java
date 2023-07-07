602d98d7e479770b78dfb959cebeb05353dd1175
Sudip Kundu <113622945+sudip-13@users.noreply.github.com>
2023-07-07 22:25:09 +0530

    Add files via upload

---
+++ area_cir.java
@@ -0,0 +1,13 @@
+class Area{
+    void area_cir(){
+        float r=5;
+        double area=3.14*r*r;
+        System.out.println(area);
+    }
+}
+public class area_cir {
+    public static void main(String[] args) {
+        Area obj=new Area();
+        obj.area_cir();
+    }
+}
