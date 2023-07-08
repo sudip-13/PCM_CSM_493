5fa299e1f7dcc256ad169eff4aaf982083849ed6
Sudip Kundu <113622945+sudip-13@users.noreply.github.com>
2023-07-08 16:02:31 +0530

    Add files via upload

---
+++ vec.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vec {
+    public static void main(String [] args){
+        Vector v=new Vector(3);
+        v.add("v");
+        v.add("ve");
+        v.add("vec");
+        System.out.println(v.size());
+        System.out.println(v.elementAt(0));
+
+    }
+}
